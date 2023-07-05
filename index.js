c6f7e4e6026bb704d4391b74093a5a43a13d99b4
EllaSEON <shineunhye5@gmail.com>
2023-07-05 14:23:47 +0900

    로그인 width Bcrypt

--- index.js
+++ index.js
@@ -44,3 +44,26 @@ app.post("/register", async (req, res) => {
     return res.status(500).json({ success: false, error: err.message });
   }
 });
+
+app.post("/login", (req, res) => {
+  // 1.요청된 이메일을 데이터 베이스에서 있는지 찾는다.
+
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다",
+      });
+    }
+    // 2. 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+      // 3. 비밀번호 까지 맞다면 토큰을 생성하기
+      user.generateToken((err, user) => {});
+    });
+  });
+});
--- models/User.js
+++ models/User.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 
+const bcrypt = require("bcrypt"); // 비밀번호 암호화
+const saltRounds = 10; // 비밀번호 해싱에 사용될 salt 길이
+
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -29,6 +32,36 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+//save 저장 되기 전에 실행되는 미들웨어
+// 사용자가 비밀번호를 변경할 때마다 해당 비밀번호를 암호화한다.
+userSchema.pre("save", function (next) {
+  var user = this;
+
+  //password 가 변경될때만 암호화해준다.
+  if (user.isModified("password")) {
+    //비밀번호를 암호화 시킨다.
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  //plainPassword 1234567  암호화된 비밀번호 $2b$10$RzvTp90gHwRHPaT1AzbzCOAgCiK6laWccCYELWtTnA8bmhE8CvavK 같은지 체크해야함
+  //1234567 도 암호화를 한다음에 암호화된 비밀번호랑 맞는지 체크해야해. 암호화된 비밀번호를 다시 1234567 plainPassword로 바꿀수는 없다
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err), cb(null, isMatch);
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
