0b65be902e2278a06a8e1b8956aa83e334f61431
EllaSEON <shineunhye5@gmail.com>
2023-07-12 10:51:33 +0900

    jwt 토큰 생성

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express"); //express 모듈 가져오기
 const app = express();
 const port = 5000; // 포트 설정
 const bodyParser = require("body-parser"); //body-parser 설정
+const cookieParser = require("cookie-parser");
 // User.js 에서 설정한 스키마 & 모델 가져오기
 const { User } = require("./models/User");
 
@@ -9,6 +10,7 @@ const config = require("./config/key");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 const uri = config.mongoURI;
@@ -34,9 +36,10 @@ app.post("/register", async (req, res) => {
     // User 모델을 사용하여 새로운 사용자 인스턴스를 생성합니다.
     const user = new User(req.body);
 
-    // 사용자 정보를 데이터베이스에 저장합니다.
+    // 사용자 정보를 데이터베이스에 저장합니다. save는 몽고DB 메서드
     await user.save();
 
+    // 성공했을 경우 success:true 로 응답
     return res.status(200).json({
       success: true,
     });
@@ -45,25 +48,37 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.post("/login", (req, res) => {
-  // 1.요청된 이메일을 데이터 베이스에서 있는지 찾는다.
+app.post("/login", async (req, res) => {
+  try {
+    // 1. 요청된 이메일을 데이터베이스에서 찾기
+    const user = await User.findOne({ email: req.body.email }).exec();
 
-  User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
       return res.json({
         loginSuccess: false,
         message: "제공된 이메일에 해당하는 유저가 없습니다",
       });
     }
-    // 2. 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
-    user.comparePassword(req.body.password, (err, isMatch) => {
-      if (!isMatch)
-        return res.json({
-          loginSuccess: false,
-          message: "비밀번호가 틀렸습니다.",
-        });
-      // 3. 비밀번호 까지 맞다면 토큰을 생성하기
-      user.generateToken((err, user) => {});
-    });
-  });
+
+    // 2. 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는지 확인
+    const isMatch = await user.comparePassword(req.body.password);
+
+    if (!isMatch) {
+      return res.json({
+        loginSuccess: false,
+        message: "비밀번호가 틀렸습니다.",
+      });
+    }
+
+    // 3. 비밀번호까지 맞다면 토큰을 생성하기
+    const token = await user.generateToken();
+
+    // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지 등
+    res
+      .cookie("x_auth", token)
+      .status(200)
+      .json({ loginSuccess: true, userId: user._id });
+  } catch (err) {
+    return res.status(500).json({ success: false, error: err.message });
+  }
 });
